17cf2f2990a05ba749b30811284e1b3b7ca284f6
EYoung75 <eyoung75.ey@gmail.com>
2018-10-30 13:23:22 -0600

    wrote patch request for API, got onClick function working to star messages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Toolbar from "./component/Toolbar.jsx";
-import MessageList from './component/MessageList.jsx';
+import Message from "./component/Message.jsx";
 
 class App extends Component {
   constructor(){
@@ -17,12 +17,43 @@ class App extends Component {
     this.setState({messages: json})
   }
 
+  patch = async (id, command, attribute, value) => {
+    var patch = {
+      messageIds: [id],
+      command: command,
+      [attribute]: value
+    }
+
+    const response = await fetch("http://localhost:8082/api/messages", {
+      method: "PATCH",
+      body: JSON.stringify(patch),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+
+    const posted = await response.json()
+    this.setState({
+      messages: posted
+    })
+  }
+
+    messageRead = (event) => {
+      this.patch([event.target.id], "read", "read", true)
+      console.log(event.target.id)
+    }
+
+    messageStarred = (event) => {
+      this.patch(event.target.id, "star", "starred")
+    }
+
 
   render() {
     return (
       <div>
         <Toolbar />
-        <MessageList messages={this.state.messages} />
+        <Message messages={this.state.messages} read={this.messageRead} star={this.messageStarred}/>
       </div>
     )
   }
--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 
-class Message extends React.Component {
-    render() {
-       
+const Message = (props) => {
+    var messages = props.messages.map(message => {
         return (
-            <div className="row message unread">
+            <>
+            <div className={`row message ${message.read ? "read" : "unread"}`} >
                 <div className="col-xs-1">
                     <div className="row">
                         <div className="col-xs-2">
                             <input type="checkbox" />
                         </div>
                         <div className="col-xs-2">
-                            <i className="star fa fa-star-o"></i>
+                            <i id={message.id} onClick={props.star} className={`star fa ${message.starred ? "fa-star" : "fa-star-o"}`}></i>
+                        {console.log(message.id)}
                         </div>
                     </div>
                 </div>
                 <div className="col-xs-11">
                     <a href="#">
-                    {this.props.message}
+                        {message.subject}
                     </a>
                 </div>
             </div>
+            </>
         )
-    }
+    })
+
+    return (
+        <div>
+            {messages}
+        </div>
+    )
 }
+
 
 export default Message;
