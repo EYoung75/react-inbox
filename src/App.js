65f42e0c459bfa40c2da2d995be38b7d62aa65e8
EYoung75 <eyoung75.ey@gmail.com>
2018-11-01 17:29:48 -0600

    added compose for button functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Toolbar from "./component/Toolbar.jsx";
 import Message from "./component/Message.jsx";
+import ComposeForm from "./component/ComposeForm.jsx";
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
       messages: [],
+      compose: false
     }
   }
 
@@ -61,11 +63,22 @@ class App extends Component {
       selected.map(message => this.patch(message.id, "read", "read", false))
     }
 
+    selectAll = (event) => {
+      this.state.messages.map(message => this.patch(message.id, "select", "select"))
+    }
+
+    compose = (event) => {
+      this.setState({compose: !this.state.compose})
+    }
+
   render() {
+
+    var compose=this.state.compose
     return (
-      <div>
-        <Toolbar readAll={this.readAll} unreadAll={this.unreadAll} messages={this.state.messages}/>
+      <div className="container">
+        <Toolbar readAll={this.readAll} unreadAll={this.unreadAll} messages={this.state.messages} selectAll={this.selectAll} compose={this.compose}/>
         <Message messages={this.state.messages} read={this.messageRead} star={this.messageStarred} onClick={this.onClick} messageSelect={this.messageSelect} selected={this.state.selected}/>
+        {compose ? <div className="container"><ComposeForm /></div> : ""}     
       </div>
     )
   }
--- src/component/Toolbar.jsx
+++ src/component/Toolbar.jsx
@@ -2,20 +2,24 @@ import React from "react";
 
 class Toolbar extends React.Component {
     render() {
+        var messages = this.props.messages
+        var allSelected = messages.every(message => message.selected === true)
+        var noneSelected =  messages.every(message => message.selected === false)
+        var unreadMessages = messages.filter(item => item.read === false).length
         return (
             <div className="row toolbar">
                 <div className="col-md-12">
                     <p className="pull-right">
-                        <span className="badge badge">2</span>
+                        <span className="badge badge">{unreadMessages}</span>
                         unread messages
                     </p>
 
-                    <a className="btn btn-danger">
+                    <a onClick={this.props.compose} className="btn btn-danger">
                         <i className="fa fa-plus"></i>
                     </a>
 
                     <button className="btn btn-default">
-                        <i className="fa fa-minus-square-o"></i>
+                        <i onClick={this.props.selectAll} className={`fa ${allSelected ? "fa-check-square-o" : noneSelected ? "fa-square-o" : "fa-minus-square-o"}`}></i>
                     </button>
 
                     <button onClick={this.props.readAll} className="btn btn-default">Mark As Read</button>
